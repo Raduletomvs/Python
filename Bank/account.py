499951e36e6890d19d16641eecfa9f2a8961c7fa
Raduletomvs <116755148+Raduletomvs@users.noreply.github.com>
2023-11-07 23:27:05 +0200

    Delete Bank directory

--- Bank/account.py
+++
@@ -1,8 +0,0 @@
-class Account:
-    def __init__(self, account_number, balance=0):
-        self.account_number = account_number
-        self.balance = balance
-
-class InvalidAccountData(Exception):
-    def __init__(self):
-        self.message = "Invalid account data provided."
--- Bank/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, ssn, name):
-        self.ssn = ssn
-        self.name = name
-        self.accounts = []
-
-class InvalidUserData(Exception):
-    def __init__(self):
-        self.message = "Invalid user data provided."
