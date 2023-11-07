c4ee6dd4d4bb86fc4c6ab5ffc8eb2deeeeea73bf
Raduletomvs <116755148+Raduletomvs@users.noreply.github.com>
2023-11-07 23:29:08 +0200

    Delete item.py

--- item.py
+++
@@ -1,9 +0,0 @@
-class Item:
-    def __init__(self, name, durability, attack=None):
-        self.name = name
-        self.durability = durability
-        self.attack = attack
-
-    def __repr__(self):
-        attack_str = str(self.attack) if self.attack is not None else "None"
-        return f"Item({self.name}, {self.durability}, {attack_str})"
